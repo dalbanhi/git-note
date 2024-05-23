008ffd960521be4c0edb42e02ec1266eb6df658a
dalbanhi <12940551+dalbanhi@users.noreply.github.com>
2024-05-22 19:53:09 -0500

    Styling fixes and no results found for cmdk

--- components/shared/LeftSidebar/CommandPalette/CommandPalette.tsx
+++ components/shared/LeftSidebar/CommandPalette/CommandPalette.tsx
@@ -41,9 +41,12 @@ const CommandPalette: React.FC<CommandPaletteProps> = ({
     <div>
       {isOpen && (
         <div className="cmdk-overlay" onClick={handleOverlayClick}>
-          <div className="cmdk-modal" onClick={(e) => e.stopPropagation()}>
-            <Command className="bg-myBlack-900">
-              <div className="flex w-full justify-between gap-2 bg-myBlack-700 p-2 text-myWhite-300">
+          <div
+            className="cmdk-modal rounded-sm"
+            onClick={(e) => e.stopPropagation()}
+          >
+            <Command className="rounded-sm bg-myBlack-900">
+              <div className="flex w-full justify-between gap-2 rounded-sm bg-myBlack-700 p-2 text-myWhite-300">
                 <div className="flex">
                   <Image
                     src="/search-icon.svg"
@@ -52,7 +55,7 @@ const CommandPalette: React.FC<CommandPaletteProps> = ({
                     height={12}
                   ></Image>
                   <Command.Input
-                    className="w-full bg-myBlack-700 p-2 text-myWhite-300"
+                    className="w-full bg-myBlack-700 p-2 text-myWhite-300 outline-none"
                     placeholder="Search..."
                     autoFocus={true}
                   />
@@ -65,7 +68,7 @@ const CommandPalette: React.FC<CommandPaletteProps> = ({
                 </button>
               </div>
 
-              <Command.List className="flex flex-col bg-myBlack-800">
+              <Command.List className=" customScroll flex h-[200px] flex-col overflow-y-scroll rounded-sm bg-myBlack-800">
                 {postFilters && (
                   <Command.Group
                     className="p-2 text-p3Reg text-myWhite-300"
@@ -76,7 +79,7 @@ const CommandPalette: React.FC<CommandPaletteProps> = ({
                         <Command.Item
                           key={filter.type}
                           //use cmdk-item class to style the items in the list
-                          className="flex items-center gap-2 p-2 text-myWhite-300 hover:bg-myBlack-700 hover:text-myWhite-100"
+                          className="flex cursor-pointer items-center gap-2 p-2 text-myWhite-300 hover:bg-myBlack-700 hover:text-myWhite-100"
                           onSelect={() => {
                             setIsOpen(false);
                             router.push(`/explore?type=${filter.type}`);
@@ -119,6 +122,12 @@ const CommandPalette: React.FC<CommandPaletteProps> = ({
                   </Command.Group>
                 )}
               </Command.List>
+
+              <Command.Empty>
+                <div className="p-2 text-p3Reg text-myWhite-300">
+                  No results found.
+                </div>
+              </Command.Empty>
             </Command>
           </div>
         </div>

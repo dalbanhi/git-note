bd2ac994b2221d666c6c34347261689280468f58
dalbanhi <12940551+dalbanhi@users.noreply.github.com>
2024-06-21 10:06:10 -0500

    added add to clipboard button - need to fix css

--- src/components/shared/Post/RenderMarkdown.tsx
+++ src/components/shared/Post/RenderMarkdown.tsx
@@ -1,15 +1,61 @@
 "use client";
 
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 
 import Markdown from "react-markdown";
+import Image from "next/image";
+import ReactDOM from "react-dom/client";
 // import our markdown and prism
 import Prism from "prismjs";
 import "@/components/shared/Post/CodeBlock/prism.css";
 
 export default function RenderMarkdown({ content }: any) {
+  //use ref to only make this happen once
+  const ref = useRef(0);
+
   useEffect(() => {
+    ref.current++;
     Prism.highlightAll();
+    console.log("rendering markdown");
+
+    const handleCopyClick = async (
+      event: React.MouseEvent<HTMLImageElement>
+    ) => {
+      console.log("copying code");
+      const codeBlock = (event.target as HTMLElement)
+        .closest("pre")
+        ?.querySelector("code");
+      if (codeBlock) {
+        try {
+          await navigator.clipboard.writeText(codeBlock.textContent || "");
+          alert("Code copied to clipboard!");
+        } catch (err) {
+          console.error("Failed to copy: ", err);
+        }
+      }
+    };
+    if (ref.current === 1) {
+      const preElements = document.querySelectorAll("pre");
+      preElements.forEach((pre) => {
+        console.log("pre element: ", pre);
+        const imageWrapper = document.createElement("div");
+        imageWrapper.classList.add("relative", "cursor-pointer", "size-10");
+        const CopyIcon = () => (
+          <Image
+            src="/icons/copy.svg"
+            alt="Copy code"
+            width={12}
+            height={12}
+            className="absolute left-0 top-0 size-full rounded-md object-contain"
+            onClick={handleCopyClick}
+          />
+        );
+        pre.appendChild(imageWrapper);
+        const root = ReactDOM.createRoot(imageWrapper);
+        root.render(<CopyIcon />);
+        pre.classList.add("flex", "justify-end");
+      });
+    }
   }, []);
 
   // render the markdown normally

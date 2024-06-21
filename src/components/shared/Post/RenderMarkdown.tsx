ef7a942e886f8ccb3a85b31d388e8cda4564aac7
dalbanhi <12940551+dalbanhi@users.noreply.github.com>
2024-06-21 10:15:30 -0500

    Added toast for copying to clipboard

--- src/components/shared/Post/RenderMarkdown.tsx
+++ src/components/shared/Post/RenderMarkdown.tsx
@@ -5,6 +5,7 @@ import { useEffect, useRef } from "react";
 import Markdown from "react-markdown";
 import Image from "next/image";
 import ReactDOM from "react-dom/client";
+import { toast, Flip } from "react-toastify";
 // import our markdown and prism
 import Prism from "prismjs";
 import "@/components/shared/Post/CodeBlock/prism.css";
@@ -12,6 +13,20 @@ import "@/components/shared/Post/CodeBlock/prism.css";
 export default function RenderMarkdown({ content }: any) {
   //use ref to only make this happen once
   const ref = useRef(0);
+
+  const showError = (message: string) => {
+    toast(message, {
+      position: "top-center",
+      autoClose: 5000,
+      hideProgressBar: true,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      theme: "colored",
+      progress: undefined,
+      transition: Flip,
+    });
+  };
 
   useEffect(() => {
     ref.current++;
@@ -28,7 +43,7 @@ export default function RenderMarkdown({ content }: any) {
       if (codeBlock) {
         try {
           await navigator.clipboard.writeText(codeBlock.textContent || "");
-          alert("Code copied to clipboard!");
+          showError("Code copied to clipboard!");
         } catch (err) {
           console.error("Failed to copy: ", err);
         }
@@ -39,21 +54,21 @@ export default function RenderMarkdown({ content }: any) {
       preElements.forEach((pre) => {
         console.log("pre element: ", pre);
         const imageWrapper = document.createElement("div");
-        imageWrapper.classList.add("relative", "cursor-pointer", "size-10");
+        imageWrapper.classList.add("relative", "cursor-pointer", "size-6");
         const CopyIcon = () => (
           <Image
             src="/icons/copy.svg"
             alt="Copy code"
             width={12}
             height={12}
-            className="absolute left-0 top-0 size-full rounded-md object-contain"
+            className="absolute left-0 top-0 my-0 size-full rounded-md object-contain"
             onClick={handleCopyClick}
           />
         );
         pre.appendChild(imageWrapper);
         const root = ReactDOM.createRoot(imageWrapper);
         root.render(<CopyIcon />);
-        pre.classList.add("flex", "justify-end");
+        pre.classList.add("flex", "justify-between");
       });
     }
   }, []);

8e076cd203af8f93f37eb4d8abf2edfdb9bf0bb3
dalbanhi <12940551+dalbanhi@users.noreply.github.com>
2024-07-19 13:43:44 -0500

    Fixed up mobile views on profile page

--- src/components/shared/MobileNav/MobileNav.tsx
+++ src/components/shared/MobileNav/MobileNav.tsx
@@ -1,9 +1,9 @@
 "use client";
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Image from "next/image";
 import Link from "next/link";
 import IconLink from "../LeftSidebar/IconLink";
-import { signOut } from "next-auth/react";
+import { getSession, signOut } from "next-auth/react";
 
 import {
   DropdownMenu,
@@ -13,71 +13,94 @@ import {
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
+import CommandPalette from "../LeftSidebar/CommandPalette/CommandPalette";
+import { getAllUserTags } from "~/lib/actions/posts";
 
 const MobileNav = () => {
+  const [isCommandPaletteOpen, setIsCommandPaletteOpen] = useState(false);
+  const [allUserTags, setAllUserTags] = useState<string[]>([]);
+
+  useEffect(() => {
+    const fetchAllUserTags = async () => {
+      const session = await getSession();
+      if (!session) return;
+      const allUserTags = await getAllUserTags(session.user.id);
+      setAllUserTags(allUserTags);
+    };
+    fetchAllUserTags();
+  }, []);
+
   return (
-    <nav className="flex justify-between bg-myBlack-800 px-3 max-sm:w-full sm:hidden">
-      <Link className="mb-6 mt-4 flex gap-2" href={"/"}>
-        <Image
-          src="/icons/gitnote.svg"
-          alt="GitNote"
-          width={20}
-          height={20}
-        ></Image>
-        <h1 className="text-h1Md text-myWhite-100 ">GitNote</h1>
-      </Link>
-      <DropdownMenu>
-        <DropdownMenuTrigger>
+    <React.Fragment>
+      <nav className="hidden bg-myBlack-800 px-3 max-md:w-full max-md:justify-between max-sm:flex">
+        <Link className="mb-6 mt-4 flex gap-2" href={"/"}>
           <Image
-            src="/icons/burger.svg"
-            alt="Menu"
+            src="/icons/gitnote.svg"
+            alt="GitNote"
             width={20}
             height={20}
           ></Image>
-        </DropdownMenuTrigger>
-        <DropdownMenuContent>
-          <DropdownMenuLabel>
-            <Link href={"/profile"}>My Profile</Link>
-          </DropdownMenuLabel>
-          <DropdownMenuSeparator />
-          <DropdownMenuItem>
-            <Link href="/note/create" className=" flex gap-2">
-              <Image
-                src="/icons/plus.svg"
-                alt="Create Note"
-                width={10}
-                height={6}
-              ></Image>
-              Create Post
-            </Link>
-          </DropdownMenuItem>
-          <DropdownMenuItem>
-            <Link href="/note/create" className=" flex gap-2">
-              <Image
-                src="/icons/search.svg"
-                alt="Search"
-                width={10}
-                height={6}
-              ></Image>
-              Search
-            </Link>
-          </DropdownMenuItem>
-          <DropdownMenuSeparator />
-          <DropdownMenuItem>
-            <IconLink
-              iconColor="text-myWhite-500"
-              iconSrc="/icons/logout.svg"
-              iconAlt="Logout"
-              textColor="text-myWhite-300"
-              text="Logout"
-              onClick={() => {
-                signOut();
-              }}
-            />
-          </DropdownMenuItem>
-        </DropdownMenuContent>
-      </DropdownMenu>
-    </nav>
+          <h1 className="text-h1Md text-myWhite-100 ">GitNote</h1>
+        </Link>
+        <DropdownMenu>
+          <DropdownMenuTrigger>
+            <Image
+              src="/icons/burger.svg"
+              alt="Menu"
+              width={20}
+              height={20}
+            ></Image>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent>
+            <DropdownMenuLabel>
+              <Link href={"/profile"}>My Profile</Link>
+            </DropdownMenuLabel>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem>
+              <Link href="/note/create" className=" flex gap-2">
+                <Image
+                  src="/icons/plus.svg"
+                  alt="Create Note"
+                  width={10}
+                  height={6}
+                ></Image>
+                Create Post
+              </Link>
+            </DropdownMenuItem>
+            <DropdownMenuItem>
+              <IconLink
+                iconColor="text-myWhite-500"
+                iconSrc="/icons/search.svg"
+                iconAlt="Search"
+                textColor="text-myWhite-300"
+                text="Search"
+                onClick={() => {
+                  setIsCommandPaletteOpen(true);
+                }}
+              />
+            </DropdownMenuItem>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem>
+              <IconLink
+                iconColor="text-myWhite-500"
+                iconSrc="/icons/logout.svg"
+                iconAlt="Logout"
+                textColor="text-myWhite-300"
+                text="Logout"
+                onClick={() => {
+                  signOut();
+                }}
+              />
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      </nav>
+      <CommandPalette
+        isOpen={isCommandPaletteOpen}
+        setIsOpen={setIsCommandPaletteOpen}
+        allUserTags={allUserTags}
+      />
+    </React.Fragment>
   );
 };
 
